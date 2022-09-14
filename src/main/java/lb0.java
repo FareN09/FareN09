ac0cb92eb9d2a1870882869b09812a325865af2e
FareN <denisryzchenko@gmail.com>
2022-09-14 17:15:04 +0300

    Checking for updates

--- src/main/java/lb0.java
+++ src/main/java/lb0.java
@@ -1,5 +1,6 @@
 public class lb0 {
     public static void main(String[] args) {
-System.out.println("Hello world");
+System.out.println("Hello world!!!");
+        System.out.println("Checking for updates");
     }
 }
